35ca0aaed56fc3e483e7f74335f1ce0ea90471b2
Anatolii_Kravchenko <tol890@ukr.net>
2021-03-13 22:16:36 +0300

    timer features: start/stop, wait, reset

---
+++ src/StopwatchDisplay.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class StopwatchDisplay extends React.Component {
+  render() {
+    return (
+      <div className={'stopwatch__display'}>
+        <span>
+          {this.props.formatTime(this.props.currentTimeHr)}:
+          {this.props.formatTime(this.props.currentTimeMin)}:
+          {this.props.formatTime(this.props.currentTimeSec)}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default StopwatchDisplay;
